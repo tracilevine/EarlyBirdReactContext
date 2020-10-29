ef260b1febe397d5dc419ed54b00fc57f1c88c89
Will <wkennedy210@gmail.com>
2020-10-29 09:42:11 -0400

    Revert "context early bird"
    This reverts commit 255ffbd8779e01d5b70ad055491df6d920120340.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
-import React from 'react';
-import { AppProvider } from './context/AppContext';
+import logo from './logo.svg';
 import './App.css';
-import Home from './components/Home';
-import About from './components/About';
 
 function App() {
   return (
-    <AppProvider>
-      <Home />
-      <About />
-    </AppProvider>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
